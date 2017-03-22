612e4c9e9ce6abe5b46c853d023de401b86a54c4
James Echmalian <ech@ech.net>
2017-03-22 19:09:36 -0400

    add call to process.exit() to callRefreshReminders

--- scheduler/callRefreshReminders.js
+++ scheduler/callRefreshReminders.js
@@ -5,7 +5,9 @@ const processReminderHandle = new RefreshReminders();
 processReminderHandle.processReminders()
 .then(function(result) {
   console.log(result);
+  process.exit(0);
 })
 .catch(function(error) {
   console.error(error);
+  process.exit(1);
 })
